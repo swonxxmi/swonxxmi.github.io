e7044407e69dd1d5c21e9a82ec6b8c0d82fc580f
swonxxmi <145468272+swonxxmi@users.noreply.github.com>
2025-04-14 13:42:14 +0800

    部署构建后的文件

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',  // 对于用户站点，base 应该是根路径
   plugins: [
     vue(),
     vueDevTools(),
